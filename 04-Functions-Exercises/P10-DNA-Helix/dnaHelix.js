5368d6b7d15bb548e61da42bb9b5a8f7251a329c
Zhelyan Radoev <jelioradoev@gmail.com>
2018-01-22 13:37:58 +0200

    added solution

--- 04-Functions-Exercises/P10-DNA-Helix/dnaHelix.js
+++ 04-Functions-Exercises/P10-DNA-Helix/dnaHelix.js
@@ -1,6 +1,26 @@
 function printDnaHelix(length) {
-    // TODO
+    let pattern = 'ATCGTTAGGG';
+    let printed = 0;
+    let counter = 0;
+    let first = '';
+    let second = '';
+
+    while (printed !== length) {
+        printed++;
+        first = pattern[counter];
+        second = pattern[counter + 1];
+        counter += 2;
+        if (counter > pattern.length - 1) counter = 0;
+
+        if (printed % 4 === 1) {
+            console.log(`**${first}${second}**`);
+        } else if (printed % 4 === 0 || printed % 4 === 2) {
+            console.log(`*${first}--${second}*`);
+        } else if (printed % 4 === 3) {
+            console.log(`${first}----${second}`);
+        }
+    }
 }
 
 // printDnaHelix(4);
-// printDnaHelix(10);
+// printDnaHelix(20);
