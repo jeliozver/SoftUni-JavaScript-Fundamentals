7b727b8db394c071101f20a4820e7f81e6f72906
Zhelyan Radoev <jelioradoev@gmail.com>
2018-01-26 09:03:03 +0200

    reworked solution

--- 05-Arrays-and-Matrices-Exercises/P10-Orbit/buildOrbits.js
+++ 05-Arrays-and-Matrices-Exercises/P10-Orbit/buildOrbits.js
@@ -4,43 +4,64 @@ function buildOrbits(args) {
 
     let num = 2;
     matrix[x][y] = 1;
-    let counter = 1;
-    let currentRow = x;
-    let currentCol = y;
 
-    while (true) {
-        let startRow = Math.max(0, currentRow - counter);
-        let endRow = Math.min(cols - 1, currentRow + counter);
-        let startCol = Math.max(0, currentCol - counter);
-        let endCol = Math.min(rows - 1, currentCol + counter);
-        let isFilled = false;
+    let rowStart = x;
+    let rowEnd = x;
+    let colStart = y;
+    let colEnd = y;
 
-        for (let i = startRow; i <= endRow; i++) {
-            for (let j = startCol; j <= endCol; j++) {
-                if (i >= rows) {
-                    break;
-                }
+    let counterRowStart = rowEnd;
+    let counterRowEnd = rowEnd;
+    let counterColStart = colEnd;
+    let counterColEnd = colEnd;
+
+    while(true)
+    {
+        counterRowEnd++;
+        counterColEnd++;
+        counterRowStart--;
+        counterColStart--;
+
+        if (counterRowEnd > rows - 1
+            && counterColEnd > cols - 1
+            && counterRowStart < 0
+            && counterColStart < 0) {
+            break;
+        }
+
+        if(rowEnd >= rows - 1) {
+            rowEnd = rows - 1;
+        } else {
+            rowEnd++;
+        }
+
+        if (colEnd >= cols - 1)  {
+            colEnd = cols - 1;
+        } else {
+            colEnd++;
+        }
+
+        if (rowStart === 0) {
+            rowStart = 0;
+        } else {
+            rowStart--;
+        }
+
+        if (colStart === 0)  {
+            colStart = 0;
+        } else {
+            colStart--;
+        }
+
+        for (let i = rowStart; i <= rowEnd; i++) {
+            for (let j = colStart; j <= colEnd; j++) {
                 if (matrix[i][j] === 0) {
                     matrix[i][j] = num;
-                    isFilled = true;
                 }
             }
         }
 
-        if (!isFilled) {
-            break;
-        }
-
         num++;
-        counter++;
-    }
-
-    if (rows - cols > 0) {
-        let diff = rows - cols;
-        fillRestRows(matrix, diff, num);
-    } else if (rows - cols < 0) {
-        let diff = cols - rows;
-        fillRestCols(matrix, diff, num);
     }
 
     for (let i = 0; i < rows; i++) {
@@ -60,33 +81,7 @@ function buildOrbits(args) {
 
         return arr;
     }
-
-    function fillRestRows(matrix, diff, num) {
-        for (let i = rows - diff; i < rows; i++) {
-            for (let j = 0; j < cols; j++) {
-                matrix[i][j] = num;
-            }
-
-            num++;
-        }
-
-        return matrix;
-    }
-    
-    function fillRestCols(matrix, diff, num) {
-        while (diff !== 0) {
-            for (let i = 0; i < rows; i++) {
-                matrix[i][cols - diff] = num;
-            }
-
-            num++;
-            diff--;
-        }
-
-        return matrix;
-    }
 }
 
-// buildOrbits([8, 8, 2, 2]);
-// buildOrbits([7, 5, 2, 2]);
-// buildOrbits([5, 6, 2, 2]);
+// buildOrbits([9, 10, 8, 9]);
+// buildOrbits([10, 9, 4, 5]);
