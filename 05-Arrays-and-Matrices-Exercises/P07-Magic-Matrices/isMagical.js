1922cd125824d7e0c5028c9f14398b0df4127002
Jeliozver <jelioradoev@gmail.com>
2018-01-26 17:12:36 +0200

    fixed bug

--- 05-Arrays-and-Matrices-Exercises/P07-Magic-Matrices/isMagical.js
+++ 05-Arrays-and-Matrices-Exercises/P07-Magic-Matrices/isMagical.js
@@ -2,15 +2,22 @@ function isMagical(matrix) {
     for (let i = 0; i < matrix.length - 1; i++) {
         let sumROne = matrix[i].reduce((a, b) => a + b, 0);
         let sumRTwo = matrix[i + 1].reduce((a, b) => a + b, 0);
+
+        if (sumROne !== sumRTwo) {
+            return false;
+        }
+
         let sumCOne = 0;
         let sumCTwo = 0;
 
         for (let j = 0; j < matrix.length; j++) {
-            sumCOne += matrix[i][j];
-            sumCTwo += matrix[i + 1][j];
+            sumCOne += matrix[j][i];
+            sumCTwo += matrix[j][i + 1];
         }
 
-        if (sumROne !== sumRTwo || sumCOne !== sumCTwo) {
+        if (sumCOne !== sumCTwo
+            || sumROne !== sumCOne
+            || sumRTwo !== sumCTwo) {
             return false;
         }
     }
